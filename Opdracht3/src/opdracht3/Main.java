9814c35778e383163bd9bdf3923847477ea23af3
Jeff <Jeff@Jefftop.lan>
2012-10-04 14:00:42 +0200

    Initial commit: Leeg project

---
+++ Opdracht3/src/opdracht3/Main.java
@@ -0,0 +1,15 @@
+package opdracht3;
+
+/**
+ *
+ * @author Jeff
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
