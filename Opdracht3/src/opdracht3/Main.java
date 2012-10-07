a43ec0d21c10d75012569436d139e23da9e1fea7
Jeff <Jeff@Jefftop.lan>
2012-10-07 16:03:47 +0200

    Ik ga nu de opdracht goed uitwerken. Hij print al goed en controleert het antwoord, maar kijkt nog niet of het daadwerkelijk een berekening is (21 werkt ook, ipv 10+10+1 bijvoorbeed)

--- Opdracht3/src/opdracht3/Main.java
+++ Opdracht3/src/opdracht3/Main.java
@@ -1,6 +1,11 @@
 package opdracht3;
 
 import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
 
 /**
  *
@@ -17,42 +22,119 @@ public class Main {
         System.out.println("Mauricio Kruijer\n");
 
         Main main = new Main();
-        main.puzzle();
+        main.puzzle(false, 10);
     }
 
-    public void puzzle() {
-        int amountOfNumbers = 4;
+    public void puzzle(boolean mayTryAgain, int numberOfQuestions) {
+        int questionsCorrect = 0;
+        for (int question = 0; question < numberOfQuestions; question++) {
+            int amountOfNumbers = 4;
 
-        int[] numbers = generateRandomNumbers(amountOfNumbers);
-        char[] operators = generateRandomOperators(amountOfNumbers - 1);
+            
+            
+            int[] numbers = generateRandomNumbers(amountOfNumbers);
+            char[] operators = generateRandomOperators(amountOfNumbers - 1);
 
-        //for(int i = 0; i < amountOfNumbers - 1; i++) {
-        //    System.out.println(operators[i]);
-        //}
-        //System.out.println(operators[0]);
+            //for(int i = 0; i < amountOfNumbers - 1; i++) {
+            //    System.out.println(operators[i]);
+            //}
+            //System.out.println(operators[0]);
 
-        String finalString = "";
-        String calculateString = "";
-        for (int i = 0; i < amountOfNumbers; i++) {
-            System.out.println(i);
-            finalString += numbers[i];
-            calculateString += numbers[i];
-            finalString += " ";
-            if (i != amountOfNumbers - 1) {
-                finalString += operators[i];
-                calculateString += operators[i];
+            String finalString = "";
+            String calculateString = "";
+            for (int i = 0; i < amountOfNumbers; i++) {
+                finalString += numbers[i];
+                calculateString += numbers[i];
                 finalString += " ";
+                if (i != amountOfNumbers - 1) {
+                    finalString += operators[i];
+                    calculateString += operators[i];
+                    finalString += " ";
+                }
             }
+            calculateString = calculateString.replaceAll("x", "*");
+            int answer = calculateAnswerFromString(calculateString);
+
+            // Order numbers from int[] numbers and display them
+            Arrays.sort(numbers);
+            for(int i = 0; i < amountOfNumbers; i++) {
+                System.out.print(numbers[i] + " ");
+            }
+            System.out.print(" ");
+            
+            // Print the operators possible
+            String operatorsPossible = null;
+            for(int i = 0; i < operators.length; i++) {
+                operatorsPossible += operators[i];
+            }
+            if(operatorsPossible.contains("+")) {
+                System.out.print("+ ");
+            }
+            if(operatorsPossible.contains("-")) {
+                System.out.print("- ");
+            }
+            if(operatorsPossible.contains("x")) {
+                System.out.print("x ");
+            }
+            System.out.println(" " + answer);
+            
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            input = input.trim();
+            
+            input.contains("+");
+            
+            int userAnswer = calculateAnswerFromString(input);
+            if(userAnswer == answer) {
+                System.out.println("Correctemente");
+            } else {
+                System.out.println("Fout");
+            }
+            
+            //System.out.print(finalString + "= ");
+            // Wait for user input
+            /*
+            Scanner in = new Scanner(System.in);
+            int input = 0;
+            boolean inputRight = false;
+            while (!inputRight) {
+                // TODO: Validate input.
+                boolean valid = false;
+                while (!valid) {
+                    try {
+                        if(in.hasNext())
+                            input = in.nextInt();
+                        
+                        valid = true;
+                    } catch (java.util.InputMismatchException ex) {
+                        System.out.println("Please enter a number.");
+                        in.reset();
+                        in.close();
+                        in = new Scanner(System.in);
+                    }
+                }
+                
+                if (input == answer) {
+                    System.out.println("You got it! Yeah!");
+                    questionsCorrect++;
+                    inputRight = true;
+                } else {
+                    if (mayTryAgain) {
+                        System.out.print("\nWrong answer. Please try again: ");
+                    } else {
+                        System.out.println("Wrong. Next question.");
+                        inputRight = true;
+                    }
+                }
+            }
+            */
         }
-        int answer = calculateAnswerFromString(calculateString);
-
-
-        System.out.println(finalString + "= " + answer);
+        System.out.println("You got " + questionsCorrect + "/" + numberOfQuestions + " correct.");
+        System.out.println("Grade: " + ((questionsCorrect/numberOfQuestions) * 10));
 
     }
 
     public int[] generateRandomNumbers(int amount) {
-        amount++;
         int[] array = new int[amount];
 
         Random rnd = new Random();
@@ -100,6 +182,21 @@ public class Main {
      */
     private int calculateAnswerFromString(String calc) {
         int answer = 0;
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        try {
+            String answerTemp = engine.eval(calc).toString();
+            answer = Integer.decode(answerTemp.substring(0, answerTemp.length() - 2));
+        } catch (ScriptException ex) {
+            System.out.println("Error in syntax! JavaScript could not evaluate "
+                             + calc);
+            System.out.println("Ex: " + ex.getMessage());
+        }
+        
+        /*
+         * Old version. This had priorities wrong (ofcourse. Just look at it, hehe)
+         * New version is above.
+         
         for (int i = 0; i < calc.length(); i++) {
             int isNumber = -1;
             isNumber = isNumber(calc.charAt(i));
@@ -121,9 +218,12 @@ public class Main {
                 }
             }
         }
+        */
         return answer;
     }
 
+
+    //<editor-fold defaultstate="collapsed" desc="For use with the old (wrong) calculation method">
     private int isNumber(char c) {
         for (int i = 0; i < 10; i++) {
             // ASCII conversion (char 0 = 48, so subtract 48)
@@ -133,4 +233,6 @@ public class Main {
         }
         return -1;
     }
+    //</editor-fold>
+    
 }
